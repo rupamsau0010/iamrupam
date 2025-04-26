2d3068bb8870213285fe20b3f7b906a0ea1279a1
Rupam Sau <{rupamsau721401@gmail.com}>
2025-04-26 23:56:05 +0530

    added base without / after

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/iamrupam/" : "/", // ✅ Dynamic base
+  base: mode === "production" ? "/iamrupam" : "/", // ✅ Dynamic base
   server: {
     host: "::",
     port: 8080,
